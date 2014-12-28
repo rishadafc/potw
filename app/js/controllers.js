6f2a80118e4312a985b93240e89fc36300c29fcf
Rishad Bharucha <rb2102@gmail.com>
2014-12-28 11:43:20 +0530

    Use videoId instead of videoUrl

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,6 +3,7 @@
 /* Controllers */
 
 var potwControllers = angular.module('potwControllers', []);
+var $baseVideoURL = "//www.youtube.com/embed/";
 
 potwControllers.controller('PlayerListCtrl', ['$scope', 'Player',
   function($scope, Player) {
@@ -11,12 +12,14 @@ potwControllers.controller('PlayerListCtrl', ['$scope', 'Player',
   }]);
 
 potwControllers.controller('PlayerDetailCtrl', ['$scope', '$sce', '$routeParams', 'Player',
-  function($scope, $sce, $routeParams, Player) {
+  function($scope, $sce, $routeParams, Player, $fullVideoUrl) {
     $scope.player = Player.get({playerId: $routeParams.playerId}, function(player) {
       $scope.mainImageUrl = player.images[0];
 
       // Video URL
-      $scope.videoUrl = $sce.trustAsResourceUrl($scope.player.videoUrl);
+      
+      $scope.fullVideoUrl = $baseVideoURL.concat($scope.player.videoId);
+      $scope.videoUrl = $sce.trustAsResourceUrl($scope.fullVideoUrl);
     });
 
     $scope.setImage = function(imageUrl) {
